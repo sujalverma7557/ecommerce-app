c244d986349851c5d43dfd7df907b1c2421cac9d
Sujal <e22cseu1464@bennett.edu.in>
2025-09-13 10:34:56 +0530

    Update backend: added controllers, routes, and updated server.js

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import cors from 'cors'
 import 'dotenv/config'
 import connectDB from './config/mongodb.js'
 import connectCloudinary from './config/cloudinary.js'
+import userRouter from './routes/userRoute.js'
 
 //App Config
 const app = express()
@@ -16,6 +17,7 @@ app.use(express.json())
 app.use(cors())
 
 //api endpoints
+app.use('/api/user',userRouter)
 
 app.get('/',(req,res)=>{
     res.send("API Working")
