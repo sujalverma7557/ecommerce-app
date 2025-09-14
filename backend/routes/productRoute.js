ee59024bec81ede55730baabd32e295a14dcce61
Sujal <e22cseu1464@bennett.edu.in>
2025-09-14 23:51:24 +0530

    Added product routes and controller, updated userController and server.js

---
+++ backend/routes/productRoute.js
@@ -0,0 +1,12 @@
+import express from 'express'
+import { listProducts, addProduct, removeProduct, singleProduct } from '../controllers/productController'
+
+const productRouter = express.Router();
+
+productRouter.post('/add', addProduct);
+productRouter.post('/remove',removeProduct);
+productRouter.post('/single',singleProduct);
+productRouter.get('/list',listProducts);
+
+export default productRouter;
+
--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import 'dotenv/config'
 import connectDB from './config/mongodb.js'
 import connectCloudinary from './config/cloudinary.js'
 import userRouter from './routes/userRoute.js'
+import productRouter from './routes/productRoute.js'
 
 //App Config
 const app = express()
@@ -16,11 +17,14 @@ connectCloudinary()
 app.use(express.json())
 app.use(cors())
 
+
 //api endpoints
 app.use('/api/user',userRouter)
+app.use('/api/product', productRouter)
 
 app.get('/',(req,res)=>{
     res.send("API Working")
 })
+
 
 app.listen(port, ()=> console.log('Server started on PORT : ' + port))
